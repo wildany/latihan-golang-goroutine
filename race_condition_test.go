39964d777ea4e023dbd63e8bff5226d00d21b2c2
Mochammad Wildany Sihab <sihabwildan3@gmail.com>
2022-08-26 15:11:44 +0700

    first commit

---
+++ race_condition_test.go
@@ -0,0 +1,26 @@
+package belajargolanggoroutine
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+//mutex bisa digunakan untuk race condition
+func TestRaceCondition(t *testing.T) {
+	x := 0
+	var mutex sync.Mutex
+	for i := 1; i <= 1000; i++ {
+		go func() {
+			for j := 1; j <= 100; j++ {
+				mutex.Lock()
+				x = x + 1
+				mutex.Unlock()
+			}
+		}()
+	}
+	time.Sleep(5 * time.Second)
+
+	fmt.Println("counter = ", x)
+}
